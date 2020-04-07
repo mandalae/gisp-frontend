778450ee3765bdd1a51b13622e0b3516989720a4
Chris Skaaning <c@rpediem.com>
2020-04-07 22:47:40 +0100

    Trying to fix acceptance tests

--- acceptance/suite.rb
+++ acceptance/suite.rb
@@ -5,7 +5,6 @@ include RSpec::Matchers
 def setup
   caps    = Selenium::WebDriver::Remote::Capabilities.send("firefox")
   @driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 end
 
 def teardown
@@ -20,6 +19,7 @@ end
 
 run do
   @driver.get 'https://dev.gisp.org.uk'
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
   wait.until { @driver.title.start_with? "COVID-19" }
   expect(@driver.title).to eql "COVID-19 GP Information Sharing Portal"
   # @driver.save_screenshot('docker_image.png')
